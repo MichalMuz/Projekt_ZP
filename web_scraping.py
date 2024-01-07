8666e9d0c7b1f7465e2073b34a1cddfce5b7c056
Filip Twardawa <i15.twardawa@gmail.com>
2024-01-07 20:23:49 +0100

    Creation of the AsyncWebScraper class and its structure

--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,10 @@
 import aiohttp
 from aiohttp.client_exceptions import ClientResponseError
 from bs4 import BeautifulSoup
+
+class AsyncWebScraper:
+    def __init__(self, url, headers=None):
+        self.url = url
+        self.headers = headers or {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
